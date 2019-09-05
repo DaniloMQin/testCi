b345fae7a61520e49f153bef9f5646b147799e90
Danilo Mombrini <54800419+DaniloMQin@users.noreply.github.com>
2019-09-05 17:33:18 +0200

    test

---
+++ functions/tests/test1.py
@@ -0,0 +1,17 @@
+import unittest
+import time
+
+class Test11(unittest.TestCase):
+    def setup(self):
+        print("Hello World aaaa")
+    def test1(self):
+        print("TEST1 starts")
+        time.sleep(2)
+        print("TEST1 ends")
+    def test2(self):
+        print("TEST2 starts")
+        time.sleep(3)
+        print("TEST2 ends")
+    def teardown(self):
+        print("Good Bye World")
+
